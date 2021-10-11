73ca0fd52aaaa3edb4667341ae6b2a37bef7c68c
Bruno Depieri Barduchi <3717722+brunocoio@users.noreply.github.com>
2021-10-11 14:36:35 -0300

    att

--- src/CollectionInterface.php
+++ src/CollectionInterface.php
@@ -1,52 +1,40 @@
 <?php
-
 namespace Live\Collection;
-
 /**
  * Collection interface
- *
  * @package Live\Collection
  */
 interface CollectionInterface
 {
     /**
      * Returns a value by index
-     *
      * @param string $index
      * @param mixed $defaultValue
      * @return mixed
      */
     public function get(string $index, $defaultValue = null);
-
     /**
      * Adds a value to the collection
-     *
      * @param string $index
-     * @param mixed $value
+     * @param $value
+     * @param int $timeVar
      * @return void
      */
-    public function set(string $index, $value);
-
+    public function set(string $index, $value, int $timeVar = 1);
     /**
      * Checks whether the collection has the given index
-     *
      * @param string $index
      * @return boolean
      */
     public function has(string $index);
-
     /**
      * Returns the count of items in the collection
-     *
      * @return integer
      */
-    public function count(): int;
-
+    //public function count(): int;
     /**
      * Cleans the collection
-     *
      * Estou aqui para testar sua atenção. Remova-me.
-     *
      * @return void
      */
     public function clean();
